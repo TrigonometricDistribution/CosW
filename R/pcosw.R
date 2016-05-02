e7fb9305c46949d2834f648c03cd9957d42e31f4
lucianoserafim <lucianoserafimdesouza@gmail.com>
2016-05-02 09:55:24 -0300

    Implementação do parametro log.p

--- R/pcosw.R
+++ R/pcosw.R
@@ -10,10 +10,20 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-pcosw<-function(q,alpha,lambda,lower = T,log.p = FALSE){
-  if (lower == T){
-    1-(sin((pi/2)*exp(-(lambda*q)^alpha)))
-  }else{
-    (sin((pi/2)*exp(-(lambda*q)^alpha)))
+pcosw<-function(q,alpha,lambda,lower = TRUE,log.p = FALSE){
+
+  if (log.p == TRUE) {
+    if (lower == TRUE){
+      log(1-(sin((pi/2)*exp(-(lambda*q)^alpha))))
+    } else {
+      log((sin((pi/2)*exp(-(lambda*q)^alpha))))
+    }
+  } else {
+    if (lower == TRUE){
+      1-(sin((pi/2)*exp(-(lambda*q)^alpha)))
+    } else {
+      (sin((pi/2)*exp(-(lambda*q)^alpha)))
+    }
   }
+
 }
--- R/qcosw.R
+++ R/qcosw.R
@@ -10,16 +10,27 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-qcosw<-function(p,alpha = 1,lambda,lower = T,log.p = FALSE){
+qcosw<-function(p,alpha = 1,lambda,lower = TRUE,log.p = FALSE){
   library(stats)
   library(pracma)
+
   if (alpha == 1){
-    if (lower == T){
-      y=(acos(1-p))
-      (-1/lambda)*(log((1-((2/pi)*y))))^(1/alpha)
-    }else{
-      y=(acos(p))
-      (-1/lambda)*(log((1-((2/pi)*y))))^(1/alpha)
+    if (log.p == TRUE) {
+      if (lower == TRUE){
+        y=(acos(1-p))
+        log((-1/lambda)*(log((1-((2/pi)*y))))^(1/alpha))
+      }else{
+        y=(acos(p))
+        log((-1/lambda)*(log((1-((2/pi)*y))))^(1/alpha))
+      }
+    } else {
+      if (lower == TRUE){
+        y=(acos(1-p))
+        (-1/lambda)*(log((1-((2/pi)*y))))^(1/alpha)
+      }else{
+        y=(acos(p))
+        (-1/lambda)*(log((1-((2/pi)*y))))^(1/alpha)
+      }
     }
   }else{
     print("The default value for the alpha parameter must be equal to 1.")
